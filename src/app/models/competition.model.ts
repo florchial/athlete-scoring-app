617a27f960a31ea4d95513e88631f1df5dbadbad
florrchialvo <mf.chialvo@gmail.com>
2022-06-20 20:56:40 -0300

    feat: category model + competition service

--- src/app/models/competition.model.ts
+++ src/app/models/competition.model.ts
@@ -1,2 +1,13 @@
+import {Category} from "./category.model";
+
 export class Competition {
+  id: string;
+  name: string;
+  category: Category;
+
+  constructor(id: string, name: string, category: Category) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+  }
 }
--- src/app/services/competitions.service.ts
+++ src/app/services/competitions.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Competition} from "../models/competition.model";
+import {Category} from "../models/category.model";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,17 @@ import { Injectable } from '@angular/core';
 export class CompetitionsService {
 
   constructor() { }
+
+  getAll(): Observable<Competition> {
+    return new Observable(subscriber => {
+      subscriber.next(new Competition("1", "Puño", new Category("1", "Infantil")));
+      subscriber.next(new Competition("2", "Puño", new Category("2", "Juvenil")));
+      subscriber.next(new Competition("3", "Arma Corta", new Category("2", "Juvenil")));
+      subscriber.next(new Competition("4", "Arma Larga", new Category("2", "Juvenil")));
+      subscriber.next(new Competition("5", "Arma Corta", new Category("3", "Adulto I")));
+      subscriber.next(new Competition("6", "Arma Larga", new Category("4", "Adulto II")));
+      subscriber.next(new Competition("7", "Taiji", new Category("3", "Adulto I")));
+      subscriber.next(new Competition("8", "Forma grupal", new Category("2", "Juvenil")));
+    })
+  }
 }
