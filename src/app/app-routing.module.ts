748f7e9e897c114ebf7f564991bb60d38889892a
florrchialvo <mf.chialvo@gmail.com>
2022-06-20 20:35:11 -0300

    feat: (WIP) basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompetitionsListComponent} from "./components/competitions-list/competitions-list.component";
+import {AthletesListComponent} from "./components/athletes-list/athletes-list.component";
+import {AddScoreComponent} from "./components/add-score/add-score.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'competitions', pathMatch: 'full' },
+  { path: 'competitions', component: CompetitionsListComponent },
+  { path: 'competitions/:id/athletes', component: AthletesListComponent },
+  { path: 'competitions/:id/athletes/:athlete/score', component: AddScoreComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
